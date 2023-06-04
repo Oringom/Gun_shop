3a26516d2886e605e8a3762712cb6247268552db
Oringom <a-stud-z@yandex.ru>
2023-06-04 17:30:36 +0700

    Добавлен Switch

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.myapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,17 +12,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
     int quantity = 0;
     Spinner spinner;
     ArrayList spinnerArrayList;
     ArrayAdapter spinnerAdapter;
-
     HashMap goodsMap;
     String goodsName;
     double price;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,47 +26,44 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         spinner = findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
-        spinnerArrayList= new ArrayList();
+        spinnerArrayList = new ArrayList();
 
         spinnerArrayList.add("Пистолеты");
         spinnerArrayList.add("Пистолеты-пулемёты");
         spinnerArrayList.add("Автоматы");
         spinnerArrayList.add("Винтовки");
         spinnerArrayList.add("Карабины");
-        spinnerArrayList.add("Пулемёты");
+        spinnerArrayList.add("Боеприпасы");
 
-        spinnerAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,spinnerArrayList);
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapter);
 
         goodsMap = new HashMap();
-        goodsMap.put("Пистолеты",500.0);
-        goodsMap.put("Пистолеты-пулемёты",1000.0);
-        goodsMap.put("Автоматы",1500.0);
-        goodsMap.put("Винтовки",2000.0);
-        goodsMap.put("Карабины",2500.0);
-        goodsMap.put("Пулемёты",3000.0);
+        goodsMap.put("Пистолеты", 500.0);
+        goodsMap.put("Пистолеты-пулемёты", 1000.0);
+        goodsMap.put("Автоматы", 1500.0);
+        goodsMap.put("Винтовки", 2000.0);
+        goodsMap.put("Карабины", 2500.0);
+        goodsMap.put("Боеприпасы", 3000.0);
     }
-
-    public void increaseQuantity(View view)
-    {
-        quantity=quantity+1;
-        TextView quantityTextView = findViewById (R.id.QuantityTextView);
-        quantityTextView.setText(""+quantity);
+    public void increaseQuantity(View view) {
+        quantity = quantity + 1;
+        TextView quantityTextView = findViewById(R.id.QuantityTextView);
+        quantityTextView.setText("" + quantity);
         TextView priceTextView = findViewById(R.id.Price_Text);
-        priceTextView.setText(""+ quantity * price);
+        priceTextView.setText("" + quantity * price);
 
     }
-    public void decreaseQuantity(View view)
-    {   quantity=quantity - 1;
-        if (quantity<0) quantity = 0;
-        TextView quantityTextView = findViewById (R.id.QuantityTextView);
-        quantityTextView.setText(""+quantity);
+    public void decreaseQuantity(View view) {
+        quantity = quantity - 1;
+        if (quantity < 0) quantity = 0;
+        TextView quantityTextView = findViewById(R.id.QuantityTextView);
+        quantityTextView.setText("" + quantity);
         TextView priceTextView = findViewById(R.id.Price_Text);
-        priceTextView.setText(""+ quantity * price);
+        priceTextView.setText("" + quantity * price);
 
     }
-
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
@@ -80,23 +72,32 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         TextView priceTextView = findViewById(R.id.Price_Text);
         priceTextView.setText("" + quantity * price);
         ImageView goodsImageView = findViewById(R.id.goodsImageView);
-        if (goodsName.equals("Автоматы")) {
-            goodsImageView.setImageResource(R.drawable.gun_1);
-        } else if (goodsName.equals("Винтовки")) {
-            goodsImageView.setImageResource(R.drawable.sr);
-        } else if (goodsName.equals("Карабины")) {
-            goodsImageView.setImageResource(R.drawable.kar);
-        } else if (goodsName.equals("Пулеметы")) {
-            goodsImageView.setImageResource(R.drawable.pul);
-        } else if (goodsName.equals("Пистолеты-пулемёты")) {
-            goodsImageView.setImageResource(R.drawable.pp);
-        } else if (goodsName.equals("Пистолеты")) {
-            goodsImageView.setImageResource(R.drawable.pist);
-        }
-    }
 
+       switch (goodsName) {
+           case "Пистолеты":
+               goodsImageView.setImageResource(R.drawable.pist);
+               break;
+           case "Пистолеты-пулемёты":
+               goodsImageView.setImageResource(R.drawable.pp);
+               break;
+           case "Автоматы":
+               goodsImageView.setImageResource(R.drawable.gun_1);
+               break;
+           case "Винтовки":
+               goodsImageView.setImageResource(R.drawable.sr);
+               break;
+           case "Карабины":
+               goodsImageView.setImageResource(R.drawable.kar);
+               break;
+           case "Боеприпасы":
+               goodsImageView.setImageResource(R.drawable.boeprip);
+               break;
+           default:
+               goodsImageView.setImageResource(R.drawable.pist);
+               break;
+              }
+             }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
     }
 }
