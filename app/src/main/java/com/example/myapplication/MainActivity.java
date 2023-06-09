c17c51db6c8085ffd900e19710e809769dc6d639
Oringom <a-stud-z@yandex.ru>
2023-06-09 15:48:57 +0700

    Добавлен Switch. Исправлен баг с кнопками которые уезжали.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,11 +23,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     HashMap goodsMap;
     String goodsName;
     double price;
+    EditText userNameEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        userNameEditText = findViewById(R.id.nameEditText);
         createSpinner();
         createMap();
     }
@@ -109,5 +115,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
              }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+    }
+    public void addToCart (View view){
+
+        Order order = new Order();
+
+        order.userName = userNameEditText.getText().toString();
+
+        order.goodsName = goodsName;
+
+        order.quantity = quantity;
+
+        order.orderPrice = quantity * price;
+
+        Intent orderIntent = new Intent(MainActivity.this,OrderActivity.class);
+        orderIntent.putExtra("userNameForIntent",order.userName);
+
+
+        startActivity(orderIntent);
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/OrderActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent receivedOrderIntent = getIntent();
+        String userName = receivedOrderIntent.getStringExtra("userNameForIntent");
+
+        TextView orderTextView = findViewById(R.id.orderTextView);
+        orderTextView.setText(userName);
+
+    }
+}
